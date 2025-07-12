d0d090e7a15687708ad8985851e7e2cfc3655df0
fjordimm <fjordimm@gmail.com>
2025-07-12 13:42:03 -0700

    got a simple rotating cube

---
+++ scripts/webglCheck.js
@@ -0,0 +1,6 @@
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if (!WebGL.isWebGL2Available()) {
+    const warning = WebGL.getWebGL2ErrorMessage();
+    console.log(`WEBGL IS NOT COMPATIBLE!\nError:\n${warning}`);
+}
